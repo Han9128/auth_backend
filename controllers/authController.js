fec6fcc4b5b00abdc0bc6974d8297bd6cf8dbc39
Shahreyar <mdshahreyarhannan@gmail.com>
2026-07-21 12:44:56 +0530

    implement password hashing with bcrypt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,18 @@
 
+const bcrypt = require('bcrypt');
+
 const User = require('../models/authModel');
 
 const register = async (req,res) => {
     try{
         const data = req.body;
+        // generate 10 rounds of salt
+        const salt = await bcrypt.genSalt(10);
+        console.log(salt);
+        // hash the password with the generated salt
+        const hashedPassword = await bcrypt.hash(data.password,salt);
+        data.password = hashedPassword;
+        console.log(data);
         const user = new User(data);
         const registeredUser = await user.save();
         res.status(201).json(registeredUser);
