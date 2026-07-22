33f31a5fafaa674458674643d647787292d51b37
Shahreyar <mdshahreyarhannan@gmail.com>
2026-07-22 12:06:55 +0530

    implement mongoose validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,25 +3,45 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/authModel');
 
-const register = async (req,res) => {
-    try{
-        const data = req.body;
-        // generate 10 rounds of salt
-        const salt = await bcrypt.genSalt(10);
-        console.log(salt);
-        // hash the password with the generated salt
-        const hashedPassword = await bcrypt.hash(data.password,salt);
-        data.password = hashedPassword;
-        console.log(data);
-        const user = new User(data);
-        const registeredUser = await user.save();
-        res.status(201).json(registeredUser);
-    } catch(err){
-        console.error(`Error in registration ${err}`);
-        res.status(400).json(`Error registering user ${err.message}`);
-    }
-}
 
+// when we hash password here then one issue happens that to validator defined in model hashed password is passed
+//  which pass the password validator becaused hashed password satisfy all constraints always. To avoid that 
+// if we use model validate function here then it solves the issue but now validate called twice once on plain password 
+// when we do data.validate() and next when we do data.save() which is ineffecient. So we use mongoose.pre('save') hook
+// const register = async (req,res) => {
+//     try{
+//         const data = new User(req.body)
+//         // validate the password
+//         await data.validate();
+//         // generate 10 rounds of salt
+//         const salt = await bcrypt.genSalt(10);
+//         console.log(salt);
+//         // hash the password with the generated salt
+//         const hashedPassword = await bcrypt.hash(data.password,salt);
+//         data.password = hashedPassword;
+//         console.log(data);
+//         const registeredUser = await data.save();
+//         res.status(201).json(registeredUser);
+//     } catch(err){
+//         console.error(`Error in registration ${err}`);
+//         res.status(400).json(`Error registering user ${err.message}`);
+//     }
+// }
+
+
+const register = async (req, res) => {
+    try {
+        const user = new User(req.body);
+
+        const registeredUser = await user.save();
+
+        res.status(201).json(registeredUser);
+    } catch (err) {
+        res.status(400).json({
+            error: err.message
+        });
+    }
+};
 
 module.exports = {
     register
--- models/authModel.js
+++ models/authModel.js
@@ -1,5 +1,7 @@
 
 const mongoose = require('mongoose');
+const validator = require('validator') // for db validation at db side it validates the data before saving
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -12,15 +14,42 @@ const userSchema = new mongoose.Schema({
         required:true,
         unique:true,
         trim:true,
-        lowercase:true
+        lowercase:true,
+        validate:{
+            validator:validator.isEmail,
+            message: "Invalid Email Format"
+        }
     },
     password:{
         type:String,
         required:true,
+        validate:{
+            validator: (value) => {
+                const result = validator.isStrongPassword(value,{
+                    minLength:8,
+                    minLowercase:1,
+                    minUppercase:1,
+                    minNumbers:1,
+                    minSymbols:1
+                })
+                return result
+            },
+            message: 'Password must be at least 8 characters long and include an uppercase letter, a number, and a special character.'
+        }
     }
 },{
     timestamps:true,
     collection: 'users'
+})
+
+userSchema.pre('save',async function (next){
+    if(!this.isModified('password')){
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+
+    // next(); this gives error in modern mongoose.
 })
 
 const User = mongoose.model('User',userSchema);
