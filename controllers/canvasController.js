35ecbb4258fbf22e97d9c73e9cd3b5f4142fea63
Shahreyar <mdshahreyarhannan@gmail.com>
2026-07-28 11:55:20 +0530

    implement create new canvas feature

--- controllers/canvasController.js
+++ controllers/canvasController.js
@@ -9,7 +9,7 @@ const getCanvases = async (req,res)=>{
                 {owner:userId},
                 {sharedWith:userId}
             ]
-        });
+        }).populate('owner','-password'); // without populte this retunres only owner id, with populate it returns the entire user info and '-password' avoid password being returned
 
         return res.status(200).json({canvases});
     } catch(error){
@@ -18,7 +18,25 @@ const getCanvases = async (req,res)=>{
 }
 
 
+const createCanvas = async (req,res)=>{
+    try{
+        const userId = req.user.id;
+        const canvas = new Canvas({
+            owner:userId,
+            sharedWith:[],
+            elements:[]
+        })
+
+        await canvas.save();
+        return res.status(201).json({message:"Canvas creates sucessfully",canvasId:canvas._id})
+    }catch(error){
+        return res.status(500).json({error:"Failed to create canvas",detail:error.message})
+    }
+}
+
+
 
 module.exports = {
-    getCanvases
+    getCanvases,
+    createCanvas
 }
--- routes/canvasRoute.js
+++ routes/canvasRoute.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const authenticateRoute = require('../middleware/authentication');
-const {getCanvases} = require('../controllers/canvasController');
+const {getCanvases,createCanvas} = require('../controllers/canvasController');
 
 router.get('/list',authenticateRoute,getCanvases);
+router.post('/create',authenticateRoute,createCanvas);
 
 
 module.exports = router;
